380907b23a0a2f32a0957163bc7eb114341b54a5
User <edgararsenyan@gmail.com>
2019-08-31 15:49:49 +0400

    new

--- app/src/main/java/com/noringerazancutyun/shared_preference_task2/MainActivity.java
+++ app/src/main/java/com/noringerazancutyun/shared_preference_task2/MainActivity.java
@@ -1,6 +1,5 @@
 package com.noringerazancutyun.shared_preference_task2;
 
-import android.accounts.Account;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import static com.noringerazancutyun.shared_preference_task2.FinalValues.PASSWORD_KEY;
 import static com.noringerazancutyun.shared_preference_task2.FinalValues.PREFERENCE;
@@ -41,22 +41,25 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pref = getSharedPreferences(PREFERENCE, MODE_PRIVATE);
-                SharedPreferences.Editor editor = pref.edit();
-                editor.putString(USERNAME_KEY, usernameText.getText().toString());
-                editor.putString(PASSWORD_KEY, passwordText.getText().toString());
-                editor.apply();
+                if(usernameText.getText()==null & passwordText.getText()== null){
+                    pref = getSharedPreferences(PREFERENCE, MODE_PRIVATE);
+                    SharedPreferences.Editor editor = pref.edit();
+                    editor.putString(USERNAME_KEY, usernameText.getText().toString());
+                    editor.putString(PASSWORD_KEY, passwordText.getText().toString());
+                    editor.apply();
 
-                Intent intent = new Intent(MainActivity.this, AccountActivity.class);
-                startActivity(intent);
+                    Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, "Username or Password Error", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
     private void isLogIn() {
         SharedPreferences preferences = getSharedPreferences(PREFERENCE, MODE_PRIVATE);
-        String text = preferences.getString(USERNAME_KEY, null);
-        if (text != null) {
+        if (preferences.getString(USERNAME_KEY, null) != null) {
             Intent intent = new Intent(MainActivity.this, AccountActivity.class);
             startActivity(intent);
         }
